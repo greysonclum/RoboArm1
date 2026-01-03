c59af413dd47e19d015236836e53ceeb676aa1d5
greyson.clum <clum.grey@gmail.com>
2026-01-03 11:42:48 -0800

    removed the adc part. servo still no work

--- Desktop Arm.py
+++ Desktop Arm.py
@@ -3,8 +3,8 @@ import time
 from adafruit_servokit import ServoKit
 import board
 import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+#import adafruit_ads1x15.ads1115 as ADS
+#from adafruit_ads1x15.analog_in import AnalogIn
 
 ############# Set channels to the number of servo channels on your kit. #############
 # 16 for PCA9685 board.
@@ -15,17 +15,17 @@ kit = ServoKit(channels=16)
 
 ############# Set up I2C for ADS1115 ##############
 
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADS object and specify the gain
-ads = ADS.ADS1115(i2c)
-ads.gain = 1 
-chan = AnalogIn(ads, ADS.P0)
+#ads = ADS.ADS1115(i2c)
+#0ads.gain = 1 
+#chan = AnalogIn(ads, ADS.P0)
 
 # Continuously print the values
 while True:
-    print(f"MQ-135 Voltage: {chan.voltage}V")
-    time.sleep(1)
+    #print(f"MQ-135 Voltage: {chan.voltage}V")
+    #time.sleep(1)
     kit.servo[0].angle = 180
     kit.continuous_servo[1].throttle = 1
     print("The angle of servo is : ", kit.servo[0].angle)
